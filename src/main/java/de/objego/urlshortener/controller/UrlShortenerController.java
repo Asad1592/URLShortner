204dc203df8e1c91962997ec4beb95b93a0ce75c
asad1592 <mailtoasad92@gmail.com>
2023-10-23 11:50:12 +0200

    Coded on UrlShortenController & UrlShortnerService

--- src/main/java/de/objego/urlshortener/controller/UrlShortenerController.java
+++ src/main/java/de/objego/urlshortener/controller/UrlShortenerController.java
@@ -2,12 +2,29 @@ package de.objego.urlshortener.controller;
 
 import de.objego.urlshortener.service.UrlShortenerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/url")
 @RequiredArgsConstructor
 public class UrlShortenerController {
+
     private final UrlShortenerService shortenerService;
 
-    // TODO: Implement the REST endpoints here
+    @PostMapping("/encode")
+    public ResponseEntity<String> encode(@RequestBody String longUrl) {
+        String shortUrlKey = shortenerService.encode(longUrl);
+        return ResponseEntity.ok(shortUrlKey);
+    }
+
+    @GetMapping("/decode/{shortUrlKey}")
+    public ResponseEntity<String> decode(@PathVariable String shortUrlKey) {
+        String longUrl = shortenerService.decode(shortUrlKey);
+        if (longUrl != null) {
+            return ResponseEntity.ok(longUrl);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- src/main/java/de/objego/urlshortener/service/UrlShortenerService.java
+++ src/main/java/de/objego/urlshortener/service/UrlShortenerService.java
@@ -2,6 +2,30 @@ package de.objego.urlshortener.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 @Service
 public class UrlShortenerService {
+
+    private final ConcurrentHashMap<String, String> urlMapping = new ConcurrentHashMap<>();
+    private final String BASE62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    public String encode(String longUrl) {
+        String shortUrlKey = base62Encode(longUrl.hashCode());
+        urlMapping.put(shortUrlKey, longUrl);
+        return shortUrlKey;
+    }
+
+    public String decode(String shortUrlKey) {
+        return urlMapping.get(shortUrlKey);
+    }
+
+    private String base62Encode(int value) {
+        StringBuilder encoded = new StringBuilder();
+        while (value != 0) {
+            encoded.append(BASE62.charAt(value % 62));
+            value /= 62;
+        }
+        return encoded.reverse().toString();
+    }
 }
