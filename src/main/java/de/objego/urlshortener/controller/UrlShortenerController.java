b75f43201bfd28065329084e7293a5c304bea407
asad1592 <mailtoasad92@gmail.com>
2023-10-23 12:41:29 +0200

    Adding AtomicLong in UrlShortnerService to generate unique IDs for each URL instead of using the hash code and adding exception handling

--- src/main/java/de/objego/urlshortener/controller/UrlShortenerController.java
+++ src/main/java/de/objego/urlshortener/controller/UrlShortenerController.java
@@ -2,8 +2,13 @@ package de.objego.urlshortener.controller;
 
 import de.objego.urlshortener.service.UrlShortenerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @RestController
 @RequestMapping("/api/url")
@@ -14,6 +19,14 @@ public class UrlShortenerController {
 
     @PostMapping("/encode")
     public ResponseEntity<String> encode(@RequestBody String longUrl) {
+        if (longUrl == null || longUrl.isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid URL");
+        }
+        try {
+            new URL(longUrl); // Validate the URL
+        } catch (MalformedURLException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid URL");
+        }
         String shortUrlKey = shortenerService.encode(longUrl);
         return ResponseEntity.ok(shortUrlKey);
     }
--- src/main/java/de/objego/urlshortener/service/UrlShortenerService.java
+++ src/main/java/de/objego/urlshortener/service/UrlShortenerService.java
@@ -1,31 +1,45 @@
 package de.objego.urlshortener.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
+@Slf4j
 @Service
 public class UrlShortenerService {
 
     private final ConcurrentHashMap<String, String> urlMapping = new ConcurrentHashMap<>();
+    private final AtomicLong idGenerator = new AtomicLong();
     private final String BASE62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
     public String encode(String longUrl) {
-        String shortUrlKey = base62Encode(longUrl.hashCode());
-        urlMapping.put(shortUrlKey, longUrl);
-        return shortUrlKey;
+        long id = idGenerator.incrementAndGet();  // Generate a unique ID
+        String encoded = base62Encode(id);
+        urlMapping.put(encoded, longUrl);  // Storing the mapping from the short URL to the long URL
+        log.info("Encoded URL: {} to Short URL Key: {}", longUrl, encoded);
+        return encoded;
     }
 
     public String decode(String shortUrlKey) {
-        return urlMapping.get(shortUrlKey);
+        String longUrl = urlMapping.get(shortUrlKey);
+
+        if(longUrl == null) {
+            log.warn("Short URL Key not found: {}", shortUrlKey);
+        } else {
+            log.info("Decoded Short URL Key: {} to Long URL: {}", shortUrlKey, longUrl);
+        }
+
+        return longUrl;
     }
 
-    private String base62Encode(int value) {
+    private String base62Encode(long value) {
         StringBuilder encoded = new StringBuilder();
         while (value != 0) {
-            encoded.append(BASE62.charAt(value % 62));
+            encoded.append(BASE62.charAt((int) (value % 62)));
             value /= 62;
         }
-        return encoded.reverse().toString();
+        return encoded.reverse().toString();  // Return the base62 encoded ID as the short URL
     }
 }
