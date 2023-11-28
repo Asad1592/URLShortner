70486c26093647be20922e553c7e3f5346568417
asad1592 <mailtoasad92@gmail.com>
2023-11-29 00:48:44 +0100

    Adding UrlResponse class

---
+++ src/main/java/de/objego/urlshortener/dto/UrlResponse.java
@@ -0,0 +1,17 @@
+package de.objego.urlshortener.dto;
+
+public class UrlResponse {
+    private String url;
+
+    public UrlResponse(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
